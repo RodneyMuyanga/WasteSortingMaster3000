2d9417dcaac70cbc5e2e4ce27490ec26661b2a41
Michella Stuhr Bech <144211387+MichellaBech@users.noreply.github.com>
2025-02-11 14:09:29 +0100

    Tilføjet nye prefabs og ScoreManager UI integration

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,15 +1,38 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 //This is just an example, i didn't attack the scrip to unity
 public class ScoreManager : MonoBehaviour
 {
-    public Text scoreText;
-    private int score = 0;
+    public static ScoreManager Instance { get; private set; }
+    public TextMeshProUGUI scoreText;
+    [SerializeField] private int score = 0;
 
-    public void UpdateScore(int value)
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public void AddScore(int value)
     {
         score += value;
-        scoreText.text = "Score: " + score;
+        Debug.Log("Score: " + score);
+       UpdateScoreUI();
+    }
+
+    public void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
     }
 }
